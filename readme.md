9e10716ccc6f49cf13b3dba7aa1a988692ce6e12
Rikard Glans <rikard@ecx.se>
2013-11-25 02:44:13 +0100

    Configureable and stuff.

--- readme.md
+++ readme.md
@@ -9,15 +9,38 @@ Put `theme-previews.el` somewhere in your `load-path` and `(require
 
 ## Status
 
-Currently very *beta* and stuff will change. I want to make the output
-directory configurable among other things.
-
-Use with caution.
+Currently **beta** and needs more testing.
 
 ## Usage
 
-Load a file you want to use for the previews (i.e a code snippet) and run `M-x
-theme-previews`. It will then go through all your installed themes and create
-a file called \<theme\>-\<buffer-name\>.html in your (at the moment) current
-directory.
+Load a file you want to use for the previews (i.e., a code snippet) and run
+`M-x theme-previews`. It will then go through all your installed themes and
+create a file called \<theme\>-\<buffer-name\>.html in the directory specified
+in `M-x customize-group RET theme-previews` (defaults to current directory.)
+
+## Notes
+
+Creating previews can take a very long time depending on how many themes you
+have installed.
+
+You can cancel at any time with `CTRL-g`.
+
+## License
+
+Copyright (C) 2013 Rikard Glans
+
+Author: Rikard Glans <rikard@ecx.se>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
--- theme-previews.el
+++ theme-previews.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rikard Glans <rikard@ecx.se>
 ;; URL: https://github.com/darrik/theme-previews
-;; Version: 0.0.1a
+;; Version: 0.0.2
 ;; Keywords: colorthemes, themes
 ;; Created: 22nd Nov 2013
 
@@ -30,49 +30,115 @@
 (when (< emacs-major-version 24)
   (error "Theme Previews only works with Emacs 24 or greater."))
 
+;; Customization
+
+(defgroup theme-previews nil
+  "Create previews of themes."
+  :group 'applications
+  :prefix "tp-")
+
+;; Configuration variables
+
+(defcustom tp-outdir "."
+  "Output directory."
+  :type 'string
+  :tag "Output directory"
+  :group 'theme-previews)
+
+(defcustom tp-badthemes
+  '(
+    tronesque   ;; Utterly messes up your mode line.
+    tronesqued
+    sw          ;; Base for a Star Wars™ based theme
+    color       ;; Emacs <24 color-theme package
+    vmalloc     ;; Makes emacs crap out on a missing "quote" face
+    assemblage  ;; Breaks with "max-lisp-eval-depth" error
+    dark-laptop ;; -^^-
+    mesa        ;; -^^-
+    )
+  "Themes known to mess things up."
+  :type 'sexp
+  :tag "Bad themes"
+  :group 'theme-previews)
+
+;; Functions
+
+;; TODO: Make more generic
+(defun tp--has-suffix? (theme)
+  "Check if theme is using suffix versions."
+  (let* ((thm    (replace-regexp-in-string "-theme" "" (format "%s" theme)))
+         (dark   (intern (format "%s-dark" thm)))
+         (light  (intern (format "%s-light" thm)))
+         (themes (custom-available-themes)))
+    (if (or (memq dark themes) (memq light themes))
+        t
+      nil)))
+
+(defun tp--isbad? (theme)
+  "Look up THEME in tp-badthemes."
+  (member theme tp-badthemes))
+
 ;; TODO: Error checking. Did we actually get a html buffer? did we actually switch? did we actually write a file?
 (defun tp--make-preview (theme)
   "Create preview file for THEME using current buffer."
   (let* ((bufname (buffer-name))
-         (outname (format "%s-%s.html" theme bufname))
+         (outname (format "%s/%s-%s.html" tp-outdir theme bufname))
          (cbuf    (current-buffer)))
     (let ((hbuf (htmlfontify-buffer)))
       (switch-to-buffer hbuf)
+      ;;; TODO: This might not be desireable on non-code texts.
+      ;; copy "body" into "pre"
+      (goto-char (point-min))
+      (kill-ring-save
+       (goto-char (- (search-forward-regexp "^body {") 1))
+       (search-forward-regexp "}$"))
+      (insert "\npre ")
+      (yank)
+      ;; write and kill
       (write-file outname)
       (kill-buffer)
       (switch-to-buffer cbuf))))
 
 (defun tp--make-previews (themes)
   "Call make-preview on each available theme."
-  (load-theme (car themes))
+  (let ((thm (car themes)))
+    (if (or (tp--isbad? thm) (tp--has-suffix? thm))
+        (tp--make-previews (remq thm themes)) ; TODO: nil check this
+      ;; else
+      (load-theme thm t) ; t UNSAFE
 
-  (let* ((current (car custom-enabled-themes))
-        (rest    (remq current themes)))
-    (tp--make-preview current)
-    (tp--reset-theme)
-    (unless (eq rest nil)
-      (tp--make-previews rest))))
+      (let* ((current (tp--current-theme))
+             (rest    (remq current themes)))
+        (tp--make-preview current)
+        (tp--reset-theme)
+        (unless (eq rest nil)
+          (tp--make-previews rest))))))
 
 (defun tp--reset-theme ()
   "Disable all loaded themes, effectively resetting to default colors."
   (mapc 'disable-theme custom-enabled-themes))
 
+(defun tp--current-theme ()
+  "Return currently loaded theme."
+  (car custom-enabled-themes))
+
 (defun tp--run ()
   "Do it!"
-  (let ((ctheme (car custom-enabled-themes)))
+  (let ((ctheme (tp--current-theme)))
     (tp--make-previews (custom-available-themes))
 
     (if (eq ctheme nil)
         (tp--reset-theme)
-      (load-theme ctheme t)))) ; We can probably safely assume that the currently loaded theme is safe.
+      (load-theme ctheme t)))) ; We can probably assume that the currently
+                               ; loaded theme is safe.
 
 (defun theme-previews ()
   "Make previews of all installed themes using current buffer."
   (interactive)
-  (message "Making previews in five seconds (cancel with CTRL-g.)")
-  (sit-for 5)
+  (message "[TP]: Making previews in five seconds (cancel with CTRL-g.)\nThis can take a very long time depending on how many themes you have installed!")
+  (sit-for 5) ; TODO: Replace with "Run: y/n" ?
   (tp--run)
-  (message "Done!"))
+  (message "[TP]: Done!"))
 
 (provide 'theme-previews)
 
